8d90a67f27d2ca17118f58771f8c8142a43a9992
ViktorKozh <viktor.ru253@gmail.com>
2023-09-24 15:49:21 +0300

    Test

--- Main.py
+++ Main.py
@@ -3,11 +3,9 @@ import random
 import matplotlib.pyplot as plt
 
 a = {}
-med = 0
 worstTime = {}
 medianTime = {}
-GraphStuff = [0]
-j = 0
+GraphStuff = [i for i in range(10, 10020, 10)]
 
 def find(n):
     for i in range(len(a)):
@@ -16,20 +14,17 @@ def find(n):
     return -1
 
 def fillArr(numOfEl):
+    a.clear()
     for i in range(numOfEl):
         a[i] = random.randint(0, 1000000)
 
 for i in range(10, 10020, 10):
-    GraphStuff.append(i)
     fillArr(i)
-    worstTime[j] = (timeit.timeit(lambda:find(10000000), number = 10)) / 10
-    j += 1
+    worstTime[i] = (timeit.timeit(lambda:find(10000000), number = 10)) / 10
 
-j = 0
 for i in range(10, 10020, 10):
     fillArr(i)
-    medianTime[j] = timeit.timeit(lambda:find(a[int(random.randint(1, i - 1))]), number = 1)
-    j += 1
+    medianTime[i] = timeit.timeit(lambda:find(a[int(random.randint(1, i - 1))]), number = 1)
 
 plt.plot(GraphStuff, worstTime.values(), linestyle='-')
 plt.plot(GraphStuff, medianTime.values(), linestyle='-')
