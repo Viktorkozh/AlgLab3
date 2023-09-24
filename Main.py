d31a5f531bc74261fc2393b99af68585f32439c5
ViktorKozh <viktor.ru253@gmail.com>
2023-09-24 15:31:22 +0300

    Doesnt work

--- Main.py
+++ Main.py
@@ -1,31 +1,29 @@
 import timeit
 import random
+import matplotlib.pyplot as plt
 
 a = {}
 med = 0
 worstTime = {}
 medianTime = {}
+GraphStuff = [0]
 j = 0
 
 def find(n):
-    Found = 0
     for i in range(len(a)):
         if a[i] == n:
-            Found = 1
             return i
-    if Found == 0:
-        return -1
+    return -1
 
 def fillArr(numOfEl):
     for i in range(numOfEl):
         a[i] = random.randint(0, 1000000)
 
 for i in range(10, 10020, 10):
+    GraphStuff.append(i)
     fillArr(i)
-    worstTime[j] = (timeit.timeit(lambda:find(1232121121), number = 10)) / 10
+    worstTime[j] = (timeit.timeit(lambda:find(10000000), number = 10)) / 10
     j += 1
-
-print(worstTime)
 
 j = 0
 for i in range(10, 10020, 10):
@@ -33,4 +31,11 @@ for i in range(10, 10020, 10):
     medianTime[j] = timeit.timeit(lambda:find(a[int(random.randint(1, i - 1))]), number = 1)
     j += 1
 
-print('Median:\n', medianTime)
+plt.plot(GraphStuff, worstTime.values(), linestyle='-')
+plt.plot(GraphStuff, medianTime.values(), linestyle='-')
+plt.xlabel('Номер числа фибоначчи')
+plt.ylabel('Среднее время выполнения (секунды)')
+plt.title('Зависимость времени выполнения от номера числа')
+plt.xticks(GraphStuff)
+plt.grid(False)
+plt.show()
