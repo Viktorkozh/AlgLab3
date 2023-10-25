c78bdf0aa4b6429de8ad1ce097fc9fbc10a61791
ViktorKozh <viktor.ru253@gmail.com>
2023-10-25 17:04:02 +0300

    2

--- Main.py
+++ Main.py
@@ -3,10 +3,11 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 a = {}
-worstTime = {}
-medianTime = {}
-GraphStuff = [i for i in range(10, 10010, 10)]
+worst_time = {}
+median_time = {}
+graph_stuff = [i for i in range(10, 10010, 10)]
 
 
 def find(n):
@@ -16,34 +17,34 @@ def find(n):
     return -1
 
 
-def fillArr(numOfEl):
+def fill_arr(numOfEl):
     a.clear()
     for i in range(numOfEl):
         a[i] = random.randint(0, 1000000)
 
 
 for i in range(10, 10010, 10):
-    fillArr(i)
-    worstTime[i] = (timeit.timeit(lambda: find(10000000), number=100)) / 100
+    fill_arr(i)
+    worst_time[i] = (timeit.timeit(lambda: find(10000000), number=100)) / 100
 
 for i in range(10, 10010, 10):
-    fillArr(i)
+    fill_arr(i)
     t = int(random.randint(1, i - 1))
-    medianTime[i] = timeit.timeit(lambda: find(a[t]), number=100) / 100
+    median_time[i] = timeit.timeit(lambda: find(a[t]), number=100) / 100
 
-A = np.vstack([GraphStuff, np.ones(len(GraphStuff))]).T
-y = np.array(list(worstTime.values()))[:, np.newaxis]
+A = np.vstack([graph_stuff, np.ones(len(graph_stuff))]).T
+y = np.array(list(worst_time.values()))[:, np.newaxis]
 alpha = np.dot(
-    (np.dot(np.linalg.inv(np.dot(A.T, A)), A.T)), np.array(list(worstTime.values()))
-)  # Взято из книги "Python Programming And Numerical Methods: A Guide For Engineers And Scientists": https://pythonnumericalmethods.berkeley.edu/notebooks/chapter16.04-Least-Squares-Regression-in-Python.html
+    (np.dot(np.linalg.inv(np.dot(A.T, A)), A.T)), np.array(list(worst_time.values()))
+)  
 
 plt.figure(1).set_figwidth(8)
 plt.xlabel("Количество элементов в массиве")
 plt.ylabel("Среднее время выполнения (секунды)")
 plt.title("Зависимость времени поиска элемента от размера массива\n(Худший случай)")
-plt.scatter(GraphStuff, worstTime.values(), s=5)
+plt.scatter(graph_stuff, worst_time.values(), s=5)
 plt.grid(False)
-plt.plot(GraphStuff, alpha[0] * np.array(list(GraphStuff)) + alpha[1], "r")
+plt.plot(graph_stuff, alpha[0] * np.array(list(graph_stuff)) + alpha[1], "r")
 
 formatted_alpha = [format(a, ".10f") for a in alpha]
 print(
@@ -52,21 +53,21 @@ print(
     "b =",
     formatted_alpha[1],
 )
-print("Worst time correlation", np.corrcoef(GraphStuff, list(worstTime.values()))[0, 1])
+print("Worst time correlation", np.corrcoef(graph_stuff, list(worst_time.values()))[0, 1])
 
-A = np.vstack([GraphStuff, np.ones(len(GraphStuff))]).T
-y = np.array(list(medianTime.values()))[:, np.newaxis]
+A = np.vstack([graph_stuff, np.ones(len(graph_stuff))]).T
+y = np.array(list(median_time.values()))[:, np.newaxis]
 alpha = np.dot(
-    (np.dot(np.linalg.inv(np.dot(A.T, A)), A.T)), np.array(list(medianTime.values()))
+    (np.dot(np.linalg.inv(np.dot(A.T, A)), A.T)), np.array(list(median_time.values()))
 )
 
 plt.figure(2).set_figwidth(8)
 plt.xlabel("Количество элементов в массиве")
 plt.ylabel("Среднее время выполнения (секунды)")
 plt.title("Зависимость времени поиска элемента от размера массива\n(Средний случай)")
-plt.scatter(GraphStuff, medianTime.values(), s=5)
+plt.scatter(graph_stuff, median_time.values(), s=5)
 plt.grid(False)
-plt.plot(GraphStuff, alpha[0] * np.array(list(GraphStuff)) + alpha[1], "r")
+plt.plot(graph_stuff, alpha[0] * np.array(list(graph_stuff)) + alpha[1], "r")
 
 formatted_alpha = [format(a, ".10f") for a in alpha]
 print(
@@ -77,7 +78,7 @@ print(
 )
 print(
     "Median time correlation", 
-    np.corrcoef(GraphStuff, list(medianTime.values()))[0, 1]
+    np.corrcoef(graph_stuff, list(median_time.values()))[0, 1]
 )
 
 plt.show()
